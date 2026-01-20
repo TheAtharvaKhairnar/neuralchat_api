458926d7b5f6e0d527bb726316e2efc6bd9dcd00
Atharva Khairnar <atharvakhairnar1505@gmail.com>
2026-01-20 23:10:18 +0530

    Database updations in schema

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const {Pool}=require('pg');
 const pool=new Pool({
     connectionString: process.env.DATABASE_URL||'postgresql://postgres:root@localhost:5432/neuralchat',
-    ssl: process.env.DATABASE_URL?{rejectUnauthorized:false}:false
+    ssl: process.env.DATABASE_URL?{rejectUnauthorized:false}:false,
+    options:"-c search_path=neural_chat,public"
 });
 module.exports=pool;
